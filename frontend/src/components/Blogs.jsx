2ba7877eef2214e58ac8f220fef2ddf79bdd25f5
Phillip Mugisa <73409892+phillipmugisa@users.noreply.github.com>
2022-04-27 10:57:00 +0300

    Editing blogs section

--- frontend/src/components/Blogs.jsx
+++ frontend/src/components/Blogs.jsx
@@ -7,7 +7,7 @@ const Blogs = () => {
     return (
         <section className="blogs container-fluid grid">
             <SectionHeading headingText={"Blog Articles"} classes="txt-secondary"/>
-            <div className="blogs-body horizontal-scroll grid">
+            <div className="blogs-body horizontal-scroll grid pd-inline-2">
                 <BlogCard blogId = {1}/>
                 <BlogCard blogId = {1}/>
                 <BlogCard blogId = {1}/>
