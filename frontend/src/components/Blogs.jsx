232c7e37f722fd668f77549ed46a51f9a647807b
Phillip Mugisa <73409892+phillipmugisa@users.noreply.github.com>
2022-04-27 11:00:28 +0300

    Update Blogs.jsx

--- frontend/src/components/Blogs.jsx
+++ frontend/src/components/Blogs.jsx
@@ -5,9 +5,9 @@ import SectionHeading  from './utils/sectionHeading';
 
 const Blogs = () => {
     return (
-        <section className="blogs container-fluid grid">
+        <section className="blogs container-fluid grid pd-inline-2">
             <SectionHeading headingText={"Blog Articles"} classes="txt-secondary"/>
-            <div className="blogs-body horizontal-scroll grid pd-inline-2">
+            <div className="blogs-body horizontal-scroll grid pd-inline-3">
                 <BlogCard blogId = {1}/>
                 <BlogCard blogId = {1}/>
                 <BlogCard blogId = {1}/>
