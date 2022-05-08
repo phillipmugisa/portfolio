1e40393243bf3685c1f734bdfb6d10eb8d272ff3
Phillip Mugisa <73409892+phillipmugisa@users.noreply.github.com>
2022-05-09 00:23:43 +0300

    Update header.jsx

--- frontend/src/components/utils/header.jsx
+++ frontend/src/components/utils/header.jsx
@@ -63,11 +63,11 @@ const Header = () => {
 
         window.addEventListener("scroll", function(){
             if  (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                headerRef.current.classList.add('elevated')
+                headerRef.current.classList.add('elevate')
             }
             else
             {
-                headerRef.current.classList.remove('elevated')
+                headerRef.current.classList.remove('elevate')
             }
         }, false);
          
