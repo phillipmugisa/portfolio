91a5f057c6763b00e629e0df7d2d5570e2c752c8
Phillip Mugisa <73409892+phillipmugisa@users.noreply.github.com>
2022-06-01 23:07:45 +0300

    adding pythoneverywhere to allowed hosts

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('PORTFOLIO_DJANGO_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["phillipmugisa03.pythoneverywhere.com",]
 
 
 # Application definition
